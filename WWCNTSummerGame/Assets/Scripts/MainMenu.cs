f623db39d67cb8b2f6bf663093a667a6f027875d
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-05-21 18:40:25 -0400

    Added a main menu

---
+++ WWCNTSummerGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadScene("Level_0");
+    }
+    public void QuitGame(){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
+
