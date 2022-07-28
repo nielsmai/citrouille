188252f1dec67e06dd4121c6135b7d764b2dfc06
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-07-27 21:24:57 -0400

    Load Cave when picking play

--- WWCNTSummerGame/Assets/Scripts/UI/MainMenu.cs
+++ WWCNTSummerGame/Assets/Scripts/UI/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene("Level_0");
+        SceneManager.LoadScene("Level_1_Cave");
     }
     public void QuitGame(){
         Debug.Log("Quit");
